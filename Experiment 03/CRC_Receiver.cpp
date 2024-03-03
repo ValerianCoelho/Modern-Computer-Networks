097740a0cea275e15951c9981090942846e6a836
Valerian Coelho <valerian27012004@gmail.com>
2024-03-03 20:30:33 +0530

    Receivers Side Algorithm Complete

---
+++ Experiment 03/CRC_Receiver.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string XOR(string num1, string num2) {
+    string result = "";
+    for (int i = 0; i < num1.length(); i++) {
+        result += num1[i] == num2[i] ? '0' : '1';
+    }
+    return result;
+}
+
+int find(string text, char ch) {
+    for(int i=0; i < text.length(); i++) {
+        if(text[i] == ch) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    string data, gox;
+    cout << "Enter the Recieved Code Word : ";
+    cin >> data;
+
+    cout << "Enter g(x) : ";
+    cin >> gox;
+
+    string temp = data.substr(0, gox.length());
+    int i = gox.length() - 1;
+    while (i < data.length()) {
+        temp = temp[0] == '1'
+              ? XOR(temp, gox) // XOR temp and g(x)
+              : temp.substr(1, temp.length()) + data[++i]; // remove fist leading zero and add one bit from data
+        cout << temp << endl;
+    }
+    if(find(temp, '1') == -1) {
+        cout << "There is no Error in the data Received";
+    } else {
+        cout << "There is an Error in the Data Received";
+    }
+    return 0;
+}
